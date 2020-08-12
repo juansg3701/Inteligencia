e9002121c6c73e420e1797dae5373bae7ab85de5
juansg3701 <54075068+juansg3701@users.noreply.github.com>
2020-08-12 11:36:56 -0500

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -11,3 +11,38 @@ $(document).on('click', '#hit', function(event) {
         },
     });
 });
+
+
+function botones(id){
+
+    alert(id);
+
+/*
+var info = "id1=" + id;
+        var xhr = new XMLHttpRequest()
+        xhr.open("POST", "/index.py", false);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded", false);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                alert("aaaa");
+                createsell = xhr.responseText;
+            }
+        };
+        xhr.send(info)
+*/
+   console.log("click");
+    $.ajax({
+        method: 'POST',
+        url: '/datos/numero',
+        data: {
+            'id1': id 
+        },
+        dataType: "json",
+        success: function(response) {
+            alert("siiiiii");
+        }
+    }); 
+ 
+
+}
+
